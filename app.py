c1261014aad98d814429353d4ffc707daab3c120
zubadestroyer1 <88352982+zubadestroyer1@users.noreply.github.com>
2023-06-28 16:11:19 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,19 @@
 import streamlit as st
 
-title = st.text_input('Movie title', 'Life of Brian')
-st.write('The current movie title is', title)
+import google.generativeai as palm
+palm_api_key = st.secrets["PALM_API_KEY"]
+
+def call_palm(prompt: str, palm_api_key: str) -> str:
+    palm.configure(api_key=palm_api_key)
+    completion = palm.generate_text(
+        model="models/text-bison-001",
+        prompt=prompt,
+        temperature=0,
+        max_output_tokens=800,
+    )
+
+    return completion.result
+
+user_question = st.text_input('Enter a question:', 'Tell me a joke.')
+answer = call_palm(prompt=user_question, palm_api_key=palm_api_key)
+st.write('Answer', answer)
