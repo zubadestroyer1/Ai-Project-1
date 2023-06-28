99626776e6e99898663d87c3c8432cb9b513ebe3
zubadestroyer1 <88352982+zubadestroyer1@users.noreply.github.com>
2023-06-28 16:23:14 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
-
+import pandas as pd
 import google.generativeai as palm
+
 palm_api_key = st.secrets["PALM_API_KEY"]
 
 def call_palm(prompt: str, palm_api_key: str) -> str:
@@ -13,6 +14,9 @@ def call_palm(prompt: str, palm_api_key: str) -> str:
     )
 
     return completion.result
+
+df = pd.read_csv("data.csv")
+st.dataframe(df)
 
 user_question = st.text_input('Enter a question:', 'Tell me a joke.')
 answer = call_palm(prompt=user_question, palm_api_key=palm_api_key)
