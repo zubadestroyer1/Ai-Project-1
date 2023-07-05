968d21afc27e2283a8825c6aeb122a50ead670c1
zubadestroyer1 <88352982+zubadestroyer1@users.noreply.github.com>
2023-07-05 15:43:09 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 import streamlit as st
 import pandas as pd
 import google.generativeai as palm
-
+import numpy as np
+from scipy.spatial.distance import cosine
+# BACKEND
 palm_api_key = st.secrets["PALM_API_KEY"]
 
 def call_palm(prompt: str, palm_api_key: str) -> str:
@@ -15,9 +17,44 @@ def call_palm(prompt: str, palm_api_key: str) -> str:
 
     return completion.result
 
-df = pd.read_csv("data.csv")
+def calculate_cosine_similarity(sentence1: str, sentence2: str) -> float:
+    """
+    Calculate the cosine similarity between two sentences.
+
+    Args:
+        sentence1 (str): The first sentence.
+        sentence2 (str): The second sentence.
+
+    Returns:
+        float: The cosine similarity between the two sentences, represented as a float value between 0 and 1.
+    """
+    # Tokenize the sentences into words
+    words1 = sentence1.lower().split()
+    words2 = sentence2.lower().split()
+
+    # Create a set of unique words from both sentences
+    unique_words = set(words1 + words2)
+
+    # Create a frequency vector for each sentence
+    freq_vector1 = np.array([words1.count(word) for word in unique_words])
+    freq_vector2 = np.array([words2.count(word) for word in unique_words])
+
+    # Calculate the cosine similarity between the frequency vectors
+    similarity = 1 - cosine(freq_vector1, freq_vector2)
+
+    return similarity
+
+df = pd.read_csv("question_answer_data_set_list.csv.csv")
+df['similarity'] = df.apply(lambda x: calculate_cosine_similarity(x['question'], user_question), axis = 1)
+df = df.sort_values(by='similarity', ascending=False)
+context = df['answers'].iloc[0:3]
 st.dataframe(df)
 
-user_question = st.text_input('Enter a question:', 'Tell me a joke.')
-answer = call_palm(prompt=user_question, palm_api_key=palm_api_key)
+# FRONTEND
+user_question = st.text_input('Enter a question:', 'Ask a question about pink river dolphins.')
+#prompt enginee
+engineered_prompt = f"""
+    Based on the context: {context}, answer the following question: {user_quesstion} with correct grammar and sentence structure.
+"""
+answer = call_palm(prompt=engineered_prompt, palm_api_key=palm_api_key)
 st.write('Answer', answer)
