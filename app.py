438ac9bdc3cc92f0dd602af9ba7658f875f6fe09
zubadestroyer1 <88352982+zubadestroyer1@users.noreply.github.com>
2023-07-18 13:56:03 -0700

    Update app.py

--- app.py
+++ app.py
@@ -199,15 +199,16 @@ if domain == "Image":
         # Inference
         pred = new_model.predict(image.reshape((1, w, h)))
         label = np.argmax(pred, axis=1)
-        st.write(f"Plant / Animal Prediction: {label}")
+        st.write(f"Classification Result: {label}")
     else:
         st.warning("Please upload a jpg/png file.")
     
     # Display assistant response in chat message container
-    with st.chat_message("assistant"):
-        st.markdown(response)
-    # Add assistant response to chat history
-    st.session_state.messages.append({"role": "assistant", "content": response})
+    if domain == "Text":
+        with st.chat_message("assistant"):
+            st.markdown(response)
+        # Add assistant response to chat history
+        st.session_state.messages.append({"role": "assistant", "content": response})
     
  
     
