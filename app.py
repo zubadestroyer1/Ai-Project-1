e23f090464f1afa33d6af57941c6784b41e9352a
zubadestroyer1 <88352982+zubadestroyer1@users.noreply.github.com>
2023-07-18 20:20:19 -0700

    Update app.py

--- app.py
+++ app.py
@@ -168,11 +168,11 @@ if domain == "Text":
         response = call_palm(prompt=engineered_prompt, palm_api_key=palm_api_key)
 
         
-    # Display assistant response in chat message container
-    with st.chat_message("assistant"):
-        st.markdown(response)
-    # Add assistant response to chat history
-    st.session_state.messages.append({"role": "assistant", "content": response})
+        # Display assistant response in chat message container
+        with st.chat_message("assistant"):
+            st.markdown(response)
+        # Add assistant response to chat history
+        st.session_state.messages.append({"role": "assistant", "content": response})
 
 elif domain == "Image":
     st.markdown(
