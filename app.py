e57631408385381e9921199a6a4c2cc219651b73
zubadestroyer1 <88352982+zubadestroyer1@users.noreply.github.com>
2023-07-18 20:17:57 -0700

    Update app.py

--- app.py
+++ app.py
@@ -167,6 +167,13 @@ if domain == "Text":
             
         response = call_palm(prompt=engineered_prompt, palm_api_key=palm_api_key)
 
+        
+    # Display assistant response in chat message container
+    with st.chat_message("assistant"):
+        st.markdown(response)
+    # Add assistant response to chat history
+    st.session_state.messages.append({"role": "assistant", "content": response})
+
 elif domain == "Image":
     st.markdown(
         """
@@ -203,12 +210,7 @@ elif domain == "Image":
         st.write(f"Classification Result: {label}")
     else:
         st.warning("Please upload a jpg/png file.")
-    
-    # Display assistant response in chat message container
-    with st.chat_message("assistant"):
-        st.markdown(response)
-    # Add assistant response to chat history
-    st.session_state.messages.append({"role": "assistant", "content": response})
+
     
  
     
