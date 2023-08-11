b386bf5faeeca5dd9f52f795ed801440b0dfe158
zubadestroyer1 <88352982+zubadestroyer1@users.noreply.github.com>
2023-08-11 15:56:10 -0700

    Update app.py

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ import tensorflow_addons as tfa
 # BACKEND
 palm_api_key = st.secrets["PALM_API_KEY"]
 openai.api_key = st.secrets["OPENAI_API_KEY"]
-df = pd.read_csv("question_answer_data_set_list.csv")
+df = pd.read_csv("question_answer_eduardo.csv")
 
 st.sidebar.title("Sidebar")
 model = st.sidebar.selectbox(
